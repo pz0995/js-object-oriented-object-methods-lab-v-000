ea8393447b97fa8bc8e7dcdb133214fea4d00d57
Tiffany Leigh <tiffanyleigh.10@gmail.com>
2018-12-02 22:33:43 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,48 @@
+function BoardMember() (name, homeState, training) {
+  this.name = name
+  this.homestate = homestate
+  this.traning = training
+}
+  // this.veto = function() {
+  //   console.log("No, I must disagree");
+  // };
+  
+  // this.approve = function() {
+  //   console.log("You can do that!");
+    
+  // };
+  // this.doCharity = function() {
+  //   console.log("I like to help people.");
+    
+  // };
+  // this.releasePressStatement = function()  {
+  //   console.log("You will see great things from Scuber.");
+    
+  // };
+  // this.sayHi = function() {
+  //   console.log("Hi, my name is ${this.name}. I am from ${this.homestate}, and I was trained in ${this.training}.");
+  
+  
+var veto = {
+return No, I must disagree
+}
+
+var approve = {
+return You can do that!
+  
+}
+
+var doCharity = {
+returns I like to help people.
+}
+
+var releasePressStatement = {
+returns You will see great things from Scuber.
+  
+}
+
+var sayHi = {
+returns "Hi, my name is <name>. I am from <homestate>, and I was trained in <training>.
+  };
+}
+
